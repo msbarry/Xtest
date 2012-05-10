d5716409e55973c9c6aed3c50d2bbef60c94c42b
Mike Barry <msb5014@gmail.com>
2012-05-10 06:09:25 -0400

    Rename runner module

--- org.xtest.runner/src/org/xtest/runner/XtestRunnerModule.java
+++ org.xtest.runner/src/org/xtest/runner/XtestRunnerModule.java
@@ -3,7 +3,7 @@ package org.xtest.runner;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 
-public class XtestRunnerMudule implements Module {
+public class XtestRunnerModule implements Module {
 
     @Override
     public void configure(Binder binder) {
